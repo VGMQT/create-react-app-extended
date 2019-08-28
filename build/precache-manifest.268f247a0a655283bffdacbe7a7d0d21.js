9c94bcfe07be80ae005366ad4b759a67604b5ad7
Vlad Gerasimovich <vlad.gerasimovich.dev@gmail.com>
2019-08-28 13:59:37 +0300

    init

---
+++ build/precache-manifest.268f247a0a655283bffdacbe7a7d0d21.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5128e8208d0b0b00986ee4ab9629672",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48535fdc082eec73cbf1",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "eecf3f560c0e2eb27c0a",
+    "url": "/static/css/main.32e62de7.chunk.css"
+  },
+  {
+    "revision": "48535fdc082eec73cbf1",
+    "url": "/static/js/2.f79d0374.chunk.js"
+  },
+  {
+    "revision": "eecf3f560c0e2eb27c0a",
+    "url": "/static/js/main.7874901e.chunk.js"
+  },
+  {
+    "revision": "cd1058452f0113a87952",
+    "url": "/static/js/runtime~main.9ac4ec79.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3a8113737b373d5bccd6f71d91408d16",
+    "url": "/static/media/OpenSans-BoldItalic.3a811373.ttf"
+  },
+  {
+    "revision": "fb7e3a294cb07a54605a8bb27f0cd528",
+    "url": "/static/media/OpenSans-ExtraBold.fb7e3a29.ttf"
+  },
+  {
+    "revision": "37f52104364c2eb5482fd85777bda0ac",
+    "url": "/static/media/OpenSans-ExtraBoldItalic.37f52104.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "c147d1302b974387afd38590072e7294",
+    "url": "/static/media/OpenSans-LightItalic.c147d130.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "f6238deb7f40a7a03134c11fb63ad387",
+    "url": "/static/media/OpenSans-RegularItalic.f6238deb.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "4f04fe541ca8be9b60b500e911b75fb5",
+    "url": "/static/media/OpenSans-SemiBoldItalic.4f04fe54.ttf"
+  }
+]);
